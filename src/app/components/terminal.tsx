00bef514d041a4a567315932af2036ca63f35a4b
bradyrhora <brady0423@gmail.com>
2025-06-14 03:55:55 -0400

    Sorted and cleaned components / styles

--- src/app/components/terminal.tsx
+++ src/app/components/terminal.tsx
@@ -1,11 +1,11 @@
 'use client'
 
-import styles from "./terminal.module.css";
+import styles from "@/app/styles/terminal.module.css";
 export default function TerminalComponent() {
 	return (
 		<div id="terminal-container" className={styles["terminal-container"]}>
 			<div id="terminal" className={styles.terminal}></div>
-			<div id="terminal-blur" className={`${styles["terminal-blur"]} ${styles.glow}`}></div>
+			<div id="terminal-blur" className={`${styles["terminal-blur"]} glow`}></div>
 		</div>
 	);
 }
