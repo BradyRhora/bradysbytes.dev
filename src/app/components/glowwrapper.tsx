2a874f1accb1dcd01ed09842bdb62014524e51ff
bradyrhora <brady0423@gmail.com>
2025-06-18 07:29:37 -0400

    Created glowwrapper and adjusted style logic

---
+++ src/app/components/glowwrapper.tsx
@@ -0,0 +1,27 @@
+import styles from "@/app/styles/main.module.css";
+
+type GlowWrapperProps = {
+    children: React.ReactNode;
+}
+
+export default function GlowWrapper({children} : GlowWrapperProps) {
+    return (
+        <div id="glow-wrapper">
+            <div
+            aria-hidden="true"
+            id="container-glow"
+            className={`glow ${styles.glow} ${styles.container} ${styles['vertical-center']}`}
+            style={{
+                filter: `blur(4px})`,
+                zIndex: 0,
+                pointerEvents: "none",
+            }}
+            >
+            {children}
+            </div>
+            <div style={{ display: "none" }} id="container" className={`${styles.container} ${styles["vertical-center"]}`}>
+            {children}
+            </div>
+        </div>
+    );
+}
