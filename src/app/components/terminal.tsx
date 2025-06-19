f56c146a5dec86471810e3639631305863df5148
bradyrhora <brady0423@gmail.com>
2025-06-19 10:33:50 -0400

    Adjusted page layout

--- src/app/components/terminal.tsx
+++ src/app/components/terminal.tsx
@@ -4,8 +4,8 @@ import styles from "@/app/styles/terminal.module.css";
 export default function TerminalComponent() {
 	return (
 		<div id="terminal-container" className={styles["terminal-container"]}>
-			<div id="terminal" className={styles.terminal}></div>
 			<div id="terminal-blur" className={`${styles["terminal-blur"]} glow`}></div>
+			<div id="terminal" className={styles.terminal}></div>
 		</div>
 	);
 }
