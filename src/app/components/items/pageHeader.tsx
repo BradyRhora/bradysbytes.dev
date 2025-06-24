5fd797aa966956ec80a3aa91d476fc41ad7eee12
bradyrhora <brady0423@gmail.com>
2025-06-24 08:26:43 -0400

    Adjust main styles and fit header better on mobile

--- src/app/components/items/pageHeader.tsx
+++ src/app/components/items/pageHeader.tsx
@@ -4,9 +4,11 @@ import styles from '@/app/styles/main.module.css';
 
 export default function PageHeader({title, parent}: {title: string, parent?: string | null}) {
     return (
-        <>
-        {parent && <CommandLink className={styles.goBack} href="/">..</CommandLink>}
-        <h1 className={styles.header}>/{title}</h1>
-        </>
+        <div className={styles.headerContainer}>
+            
+            <h1 className={styles.header}>
+                {parent && <CommandLink className={styles.goBack} href="/">..</CommandLink>}/{title}
+            </h1>
+        </div>
     );
 }
